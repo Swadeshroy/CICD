ada5f92f65a4ced5006e086d434e7712c3565134
Swadeshroy <91718699+Swadeshroy@users.noreply.github.com>
2023-04-12 07:08:03 +0000

    Delete start_server.sh

--- start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Bash script to install Apache httpd Server in Enterprise linux  7.x
-#Run this script as root 
-
-set -e   # stop script execution on any error
-
-#Start httpd Server an set restart automatically after reboot
-systemctl start httpd
